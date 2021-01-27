d69696598d544f2c4dc1e6f8200ae9cc432bf794
MRoss <mross20@myseneca.ca>
2021-01-27 15:56:49 -0500

    Chakra UI, Absolute Imports, Firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import { CSSReset, ThemeProvider } from '@chakra-ui/react';
+import { Global, css } from '@emotion/core';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { AuthProvider } from '@/lib/auth';
+import customTheme from '@/styles/theme';
 
-export default MyApp
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;

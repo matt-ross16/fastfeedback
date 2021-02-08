a51e94add32a0718deaad33e32299201ca60127e
MRoss <mross20@myseneca.ca>
2021-02-08 17:43:19 -0500

    Add Firebase Admin, SWR, and fetch all sites Restore missing files Hello world! Chakra UI, Absolute Imports, Firestore (#2) * Chakra UI, Absolute Imports, Firestore * Delete package-lock.json * Fixed index.js import * Fixed _app.js import Creating the Dashboard Shell (#3) * Chakra UI, Absolute Imports, Firestore * Delete package-lock.json * Fixed index.js import * Fixed _app.js import * Start creating the dashboard shell

---
+++ pages/dashboard.js
@@ -0,0 +1,30 @@
+import useSWR from 'swr';
+
+import EmptyState from '@/components/EmptyState';
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import DashboardShell from '@/components/DashboardShell';
+import fetcher from '@/utils/fetcher';
+//import { useAuth } from '@/lib/auth';
+import SiteTable from '@/components/SiteTable';
+
+const Dashboard = () => {
+  //const auth = useAuth();
+  const { data } = useSWR('/api/sites', fetcher);
+
+  console.log(data);
+
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
+  }
+  return (
+    <DashboardShell>
+      {data.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+    </DashboardShell>
+  );
+};
+
+export default Dashboard;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,34 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { Button, Code, Text, Icon, Flex } from '@chakra-ui/react';
 
-export default function Home() {
+import { useAuth } from '@/lib/auth';
+import EmptyState from '@/components/EmptyState';
+
+
+const Home = () => {
+  const auth = useAuth();
   return (
-    <div className={styles.container}>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Fast Feedback</title>
       </Head>
+      <Icon color="black" name="logo" size="64px" />
+      {auth.user ? (
+        <EmptyState />
+      ) : (
+        // <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+        <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
+          Sign In
+        </Button>
+      )}
+    </Flex>
+  );
+};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default Home;
